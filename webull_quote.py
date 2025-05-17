a798b4bc30a6439faf0c94d694a644537d5a4373
geneko <geneko@gmail.com>
2025-05-17 14:06:17 -0400

    fixed missing imports

--- webull_quote.py
+++ webull_quote.py
@@ -7,10 +7,10 @@ import json
 import argparse
 import subprocess
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.safari.service import Service
 from selenium.webdriver.common.by import By
-#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 import logging
 
 numbers_file = "retirement plan.numbers"
