a3493de61bae648573bf88234348bbdc8b8d7f3b
mrgeneko <geneko@gmail.com>
2025-05-21 10:20:46 -0400

    adjust sleep times

--- scrapeman_bonds.sh
+++ scrapeman_bonds.sh
@@ -2,5 +2,5 @@
 open /Users/gene/Library/Mobile\ Documents/com\~apple\~Numbers/Documents/Shared\ with\ Sharon/retirement\ plan.numbers
 sleep 5
 
-python3 scrapeman.py --include-type bonds -s 30 -l INFO
+python3 scrapeman.py --include-type bonds -l INFO
 #python3 scrapeman.py --include-type bonds -s 30 > scrapeman_bonds.log 2>&1
