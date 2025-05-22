7d74d8303618f6c4195c9eab536471cf753d7379
mrgeneko <geneko@gmail.com>
2025-05-22 16:32:59 -0400

    safari driver would not work from crontab. switch back to chrome driver

--- scrapeman_bonds.sh
+++ scrapeman_bonds.sh
@@ -2,5 +2,8 @@
 open /Users/gene/Library/Mobile\ Documents/com\~apple\~Numbers/Documents/Shared\ with\ Sharon/retirement\ plan.numbers
 sleep 5
 
-python3 scrapeman.py --include-type bonds -l INFO
-#python3 scrapeman.py --include-type bonds -s 30 > scrapeman_bonds.log 2>&1
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+python3 scrapeman.py -t bonds > /Users/gene/logs/scrapeman_bonds.${TIMESTAMP}.log 2>&1
+
+#/Library/Frameworks/Python.framework/Versions/3.13/bin/python3/python3 scrapeman.py -t bonds > /Users/gene/logs/scrapeman_bonds.${TIMESTAMP}.log 2>&1
+
