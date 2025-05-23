1c05b0b09a1bcd113d2ce881bdde688554b3411e
mrgeneko <geneko@gmail.com>
2025-05-23 14:50:05 -0400

    fix missing key value

--- scrapeman_stocks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-open /Users/gene/Library/Mobile\ Documents/com\~apple\~Numbers/Documents/Shared\ with\ Sharon/retirement\ plan.numbers
-sleep 5
-
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-/Users/gene/venv/bin/python3 scrapeman.py -t stocks > /Users/gene/logs/scrapeman_stocks.${TIMESTAMP}.log 2>&1
