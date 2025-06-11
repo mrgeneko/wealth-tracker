e66aa6ed581e977ae4cf0b34463a523ed80d3884
mrgeneko <geneko@gmail.com>
2025-06-11 09:18:03 -0400

    removed kill commands

--- scrapeman.py
+++ scrapeman.py
@@ -222,14 +222,12 @@ def main():
 
                 # Close any running instances of Safari
                 try:
-                    subprocess.run(['pkill', 'Safari'],)
-                    print("Closed all Safari instances.")
+                    #subprocess.run(['pkill', 'Safari'],)
+                    #print("Closed all Safari instances.")
 
                     process = subprocess.run(["osascript", "-e", 'quit app "Safari"'], capture_output=True, text=True)
                     if process.returncode != 0:
                         logging.error("AppleScript error:", process.stderr)
-                        return None
-                    return process.stdout.strip()
 
                 except Exception as e2:
                     print(f"Error closing Safari: {e2}")
