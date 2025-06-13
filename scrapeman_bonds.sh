225caa225c717f262640b2918bc9d7f156e6bb8d
geneko <geneko@gmail.com>
2025-06-13 14:44:25 -0400

    adjust log file name

--- scrapeman_bonds.sh
+++ scrapeman_bonds.sh
@@ -4,5 +4,5 @@ open ~/Library/Mobile\ Documents/com\~apple\~Numbers/Documents/Shared\ with\ Sha
 sleep 10
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-~/venv/bin/python3 scrapeman.py -t bonds > ~/logs/scrapeman_webull_bonds.${TIMESTAMP}.log 2>&1
+~/venv/bin/python3 scrapeman.py -t bonds > ~/logs/scrapeman_bonds.${TIMESTAMP}.log 2>&1
 
