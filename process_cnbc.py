b50ef365ab56fbdacd26fdacada8635e9688ed1c
geneko <geneko@gmail.com>
2025-06-15 10:59:00 -0400

    clean up obsolete code

--- process_cnbc.py
+++ process_cnbc.py
@@ -9,7 +9,6 @@ def get_cnbc_attributes():
     attributes = {
         "name" : "cnbc",
         "download" : "selenium",
-        "process" : process_cnbc,
         "extract" : extract_cnbc,
         "has_realtime" : True, 
         "has_pre_market" : True,
@@ -77,90 +76,4 @@ def extract_cnbc(ticker,html_content):
     data["source"] = "cnbc"
     
     logging.info(data)
-    return data
-
-def process_cnbc(driver,tickers):
-    logging.debug(f"process cnbc")
-    url_selection = 'cnbc'
-    for i, ticker in enumerate(tickers):
-
-        if url_selection in ticker:
-            logging.debug(f"Key {ticker['key']} has url: {ticker[url_selection]}")
-        else:
-            logging.debug(f"Key {url_selection} does not exist in this object.")
-
-        if not pd.isna(ticker[url_selection]):
-            logging.debug(f"Key {ticker['key']} has value: {ticker[url_selection]}")
-        else:
-            logging.debug(f"Key {url_selection} does not have a value or has NaN.")
-            continue
-
-        url = ticker[url_selection]
-        key = ticker['key']
-        logging.info("\n")
-        logging.info(f'Begin processing: {ticker['key']} selected url: {url}')
-        
-        driver.get(url)
-        #logging.info(f'sleep 3 seconds to allow website to load')
-        time.sleep(2)
-
-        # Wait for a specific element to be present (e.g., an element with ID 'example')
-        #wait = WebDriverWait(driver, 10)
-        #element = wait.until(EC.presence_of_element_located((By.ID, 'example')))
-        
-        html_content = driver.page_source
-    
-        logging.info(f"html_content: {html_content[:20]}")
-
-        soup = BeautifulSoup(html_content, 'html.parser')
-        # class name is misleading?
-        last_price = ""
-        after_hours_price = ""
-        regular_trading_price_element = soup.select_one('[class="QuoteStrip-dataContainer QuoteStrip-extendedHours"]') 
-        if regular_trading_price_element != None:
-            logging.info(f"found regular_trading_price_element")
-            #price_normal_element = regular_trading_price_element.select_one('[class="price-normal"]')
-            last_price_element = regular_trading_price_element.select_one('[class="QuoteStrip-lastPrice"]')
-            if last_price_element != None:
-                last_price = last_price_element.text.strip()
-                logging.info(f'last price: {last_price}')
-
-                after_hours_price_section = soup.select_one('[class="QuoteStrip-extendedDataContainer QuoteStrip-dataContainer"]') 
-                #price_normal_element = regular_trading_price_element.select_one('[class="price-normal"]')
-                after_hours_price_element = after_hours_price_section.select_one('[class="QuoteStrip-lastPrice"]')
-                after_hours_label = after_hours_price_section.select_one('[class="QuoteStrip-extendedLabel"]')
-                if after_hours_label != None:
-                    if after_hours_label.text.startswith("After Hours"):
-                        logging.info(f"we found after hours label")
-                if after_hours_price_element != None:
-                    after_hours_price = after_hours_price_element.text.strip()
-                    logging.info(f'after hours price: {after_hours_price}')
-        else:
-            logging.info(f"no found regular_trading_price_element")
-            regular_trading_price_element = soup.select_one('[class="QuoteStrip-lastPriceStripContainer"]')
-            if regular_trading_price_element != None:
-                last_price_element = regular_trading_price_element.select_one('[class="QuoteStrip-lastPrice"]')
-                last_price = last_price_element.text
-                logging.info(f'last price: {last_price}')
-
-        
-        #logging.info(f"after_hours_price: {after_hours_price}")
-
-        data = {}
-        data["key"] = key
-        data["last_price"] = last_price
-        if after_hours_price != "":
-            if is_pre_market_session():
-                logging.info(f"currently in pre market session")
-                data["pre_market_price"] = after_hours_price
-            elif is_after_hours_session():
-                logging.info(f"currently in after hours session")
-                data["after_hours_price"] = after_hours_price
-        else:
-            data["pre_market_price"] = ""
-            data["after_hours_price"] = ""
-        data["source"] = "cnbc"
-        
-        logging.info(data)
-        
     return data
--- process_moomoo.py
+++ process_moomoo.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
-import os
 import logging
 from bs4 import BeautifulSoup
-import time
-import datetime
-from subprocess import run, CalledProcessError,  CompletedProcess
-import pandas as pd
-from is_number import is_number
 
 #from selenium.webdriver.support.wait import WebDriverWait
 #from selenium.webdriver.support import expected_conditions as EC
@@ -15,7 +9,6 @@ def get_moomoo_attributes():
     attributes = {
         "name" : "moomoo",
         "download" : "selenium",
-        "process" : process_moomoo,
         "extract" : extract_moomoo,
         "has_realtime" : True, 
         "has_pre_market" : True,
@@ -27,80 +20,6 @@ def get_moomoo_attributes():
     }
     return attributes
 
-
-def process_moomoo(driver,tickers,function_handlers,sleep_interval):
-    logging.info(f"load_url_moomoo")
-    obsolete='''
-    url_selection = 'moomoo'
-    for i, ticker in enumerate(tickers):
-
-        if url_selection in ticker:
-            logging.debug(f"Key {ticker['key']} has url: {ticker[url_selection]}")
-        else:
-            logging.debug(f"Key {url_selection} does not exist in this object.")
-
-        if not pd.isna(ticker[url_selection]):
-            logging.debug(f"Key {ticker['key']} has value: {ticker[url_selection]}")
-        else:
-            logging.debug(f"Key {url_selection} does not have a value or has NaN.")
-            continue
-
-        url = ticker[url_selection]
-        key = ticker['key']
-        logging.info("\n")
-        logging.info(f'Begin processing: {ticker['key']} selected url: {url}')
-        
-        if driver != None:
-            driver.get(url)
-            #logging.info(f'sleep 3 seconds to allow website to load')
-            time.sleep(2)
-            html_content = driver.page_source
-        else:
-            logging.info(f"mode SINGLEFILE")
-            # let's use singlefile command to retrieve html
-            # Generate timestamp in YYYYMMDD_HHMMSS format
-            timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-
-            # Get the full path to your directory (this is the key fix)
-            directory = os.path.expanduser('~/investing_com_html')  # Expands ~ to full path
-            if not os.path.exists(directory):
-                os.makedirs(directory)  # Create directory if it doesn't exist
-
-            #output_file = f"~/investing_com_html/{ticker['key']}_{url_selection}.{timestamp}.html"
-            filename = os.path.join(directory, f"{ticker['key']}.moomoo.{timestamp}.html")
-
-            # Define the command and its arguments
-            #command = ["docker", "run", "--rm", "singlefile"]
-            command = ["/Users/chewie/bin/html_to_singlefile.sh"]
-            args = [key, url_selection, timestamp, url]
-
-            
-            try:
-                # Execute the command and redirect output to the file
-                logging.info(f"run save_html_to_singlefile.sh command")
-                result = run(
-                    [*command, *args],
-                    #stdout=open(filename, "w", encoding="utf-8"),
-                    check=True
-                )
-                logging.info(f"done singlefile command")
-            except CalledProcessError as e:
-                print(f"Error occurred while running the command: {e}")
-
-            #time.sleep(3)
-            logging.info(f"now writing html file")
-            try:
-                with open(filename, 'r', encoding='utf-8') as file:
-                    html_content = file.read()
-            except FileNotFoundError:
-                logging.error(f"Error: File not found {filename}")
-                return 1
-            except UnicodeDecodeError:
-                logging.error(f"Error: Could not decode the {filename} file with UTF-8 encoding")
-                return 1
-
-        extract_moomoo(key,url,html_content,function_handlers)
-'''
 
 def extract_moomoo(ticker,html_content):
     logging.info(f"extract moomoo")
