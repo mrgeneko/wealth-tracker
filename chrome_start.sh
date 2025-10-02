c70737d551611bcdc6fa2f60e32085524a8a03ed
mrgeneko <geneko@gmail.com>
2025-10-02 16:11:03 -0400

    remove url from chrome_start to fix dup browsertab

--- chrome_start.sh
+++ chrome_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-open -a /Applications/Google\ Chrome.app "https://www.investing.com/portfolio/?portfolioID=ODs%2FaGE1ZjNjM2tmN2Fkbw%3D%3D"
+#open -a /Applications/Google\ Chrome.app "https://www.investing.com/portfolio/?portfolioID=ODs%2FaGE1ZjNjM2tmN2Fkbw%3D%3D"
+open -a /Applications/Google\ Chrome.app
