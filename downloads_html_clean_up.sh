d284429981ecb7e9055c2a24da6f9a45a3db9b85
mrgeneko <geneko@gmail.com>
2025-10-02 13:57:13 -0400

    new version of clean up script

--- downloads_html_clean_up.sh
+++ downloads_html_clean_up.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Delete files matching the pattern that are more than 12 hours old
-#find $HOME/Downloads -name "portfoliowatchlist202*.html.old" -mmin +720 | xargs rm -f
-#find $HOME/Downloads -name "portfoliowatchlist202*(*).html" -mmin +720 | xargs rm -f
-
-
 # ------------------------------------------------------------------
 #  Delete old portfoliowatchlist*.html files
 #
