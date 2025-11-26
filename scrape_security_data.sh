100f0f23fa806dd5f5f3ee0c23eaeca6d0c7b4d8
geneko <geneko@gmail.com>
2025-11-26 15:49:44 -0500

    major cleanup.

--- scrape_security_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run the Investing.com monitor script in the background with proper redirection
-
-docker compose exec -T scrapers nohup node scrape_security_data.js > nohup.out 2>&1 < /dev/null &
